5f2a47373889ba5fc6f1ecf36024d181f9546a57
Anwesh43 <anweshthecool0@gmail.com>
2019-05-26 10:56:52 +0530

    adding CircleJoinTriangleView class

--- circlejointriangleview/src/main/java/com/anwesh/uiprojects/circlejointriangleview/CircleJoinTriangleView.kt
+++ circlejointriangleview/src/main/java/com/anwesh/uiprojects/circlejointriangleview/CircleJoinTriangleView.kt
@@ -63,3 +63,21 @@ fun Canvas.drawCJTNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class CircleJoinTriangleView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
