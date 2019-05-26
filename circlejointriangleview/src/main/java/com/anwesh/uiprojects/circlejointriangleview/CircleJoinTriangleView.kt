2df54766afb4317eed6c2c0203ab28d3424652c0
Anwesh43 <anweshthecool0@gmail.com>
2019-05-26 10:55:22 +0530

    adding drawCTJNode canvas extension method

--- circlejointriangleview/src/main/java/com/anwesh/uiprojects/circlejointriangleview/CircleJoinTriangleView.kt
+++ circlejointriangleview/src/main/java/com/anwesh/uiprojects/circlejointriangleview/CircleJoinTriangleView.kt
@@ -13,8 +13,9 @@ import android.content.Context
 import android.app.Activity
 
 val nodes : Int = 5
-val lines : Int = 4
+val lines : Int = 2
 val scGap : Float = 0.05f
+val parts : Int = 2
 val scDiv : Double = 0.51
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
@@ -30,3 +31,35 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawCircleJoin(sc : Float, size : Float, paint : Paint) {
+    val lineUpdated : Float = size * sc.divideScale(1, parts)
+    save()
+    drawLine(0f, 0f, size * sc.divideScale(0, parts), 0f, paint)
+    drawLine(size, 0f, size - lineUpdated, -lineUpdated, paint)
+    restore()
+}
+
+fun Canvas.drawCJTNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(90f * sc2)
+    drawCircle(0f, 0f, size, paint)
+    for (j in 0..(lines -1)) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawCircleJoin(sc1.divideScale(j, lines), size, paint)
+        restore()
+    }
+    restore()
+}
