8734f81ad0f6146d80982c0925fe6957f9e84854
Anwesh43 <anweshthecool0@gmail.com>
2019-05-26 11:17:06 +0530

    adding CJTNode class

--- circlejointriangleview/src/main/java/com/anwesh/uiprojects/circlejointriangleview/CircleJoinTriangleView.kt
+++ circlejointriangleview/src/main/java/com/anwesh/uiprojects/circlejointriangleview/CircleJoinTriangleView.kt
@@ -128,4 +128,48 @@ class CircleJoinTriangleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CJTNode(var i : Int, val state : State = State()) {
+
+        private var prev : CJTNode? = null
+        private var next : CJTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CJTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCJTNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CJTNode {
+            var curr : CJTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
